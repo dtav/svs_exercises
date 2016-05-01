1d4a0e6ded29664f4b5baf068abe25784bc47ef9
dtav <tavcioski.danail@hotmail.com>
2016-05-01 20:47:12 +0200

    TimeUtils method
    convert string to days with defaults

--- twitterApp/src/main/java/com/svs/util/TimeUtils.java
+++ twitterApp/src/main/java/com/svs/util/TimeUtils.java
@@ -6,13 +6,41 @@ import java.util.Date;
 
 import org.apache.commons.lang3.time.DateUtils;
 
+
 public class TimeUtils {
 	
-	public static Date getFullDateFromDay(String day) throws ParseException {
+	public static Date getFullDateFromDay(String partDate) {
 		int[] todayMonthYear = TimeUtils.getTodaysMonthAndYear();
-		String fullString = day+"/"+todayMonthYear[0]+"/"+todayMonthYear[1];
-		Date d = DateUtils.parseDateStrictly(fullString, "dd/MM/yyyy");
+		String fullString = null;
+		String[] subStr = partDate.split(",");
+		int realLength = 0;
+		if (subStr[subStr.length-1].trim().length() > 0){
+			realLength = subStr.length;
+		} else {
+			realLength = subStr.length -1;
+		}
+		if (realLength == 3) {
+			fullString = subStr[0] + "/" + subStr[1] + "/" + subStr[2];
+		}
+		if (realLength == 2) {
+			fullString = subStr[0] + "/" + subStr[1] + "/" + todayMonthYear[1];
+		}
+		if (realLength == 1) {
+			fullString = subStr[0] + "/" + todayMonthYear[0] + "/" + todayMonthYear[1];
+		}
+
+		Date d = null;
+		try {
+			d = DateUtils.parseDateStrictly(fullString, "dd/MM/yyyy");
+
+		} catch (ParseException e) {
+			System.out.println("Error in arguments parsing!");
+		} catch (IllegalArgumentException e1) {
+			System.out.println("Error in arguments length!");
+
+		}
 		return d;
+
 	}
 	
 	public static int[] getTodaysMonthAndYear(){
@@ -22,7 +50,7 @@ public class TimeUtils {
 		cal.setTime(d);
 		int year = cal.get(Calendar.YEAR);
 		int month = cal.get(Calendar.MONTH);
-		monthYear[0] = month+1; //months start with 0 ffs
+		monthYear[0] = month+1; //months start from 0 ffs
 		monthYear[1] = year;
 		
 		return monthYear;
